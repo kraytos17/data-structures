eab93522e9b32e68fc22274bb14e5a3689cc4273
Soumil <soumilkumar903@gmail.com>
2024-08-18 13:45:28 +0530

    use fixed size buffer instead of Vec

--- src/circular_buffer.rs
+++ src/circular_buffer.rs
@@ -1,21 +1,15 @@
-use std::iter::FromIterator;
-
 #[derive(Debug)]
-pub struct CircularBuffer<T> {
-    buffer: Vec<T>,
-    capacity: usize,
+pub struct CircularBuffer<T, const N: usize> {
+    buffer: [T; N],
     head: usize,
     tail: usize,
     size: usize,
 }
 
-impl<T> CircularBuffer<T> {
-    pub fn new(capacity: usize) -> Self {
-        assert!(capacity > 0, "Capacity must be greater than 0");
-
+impl<T: Default + Copy, const N: usize> CircularBuffer<T, N> {
+    pub fn new() -> Self {
         Self {
-            buffer: Vec::with_capacity(capacity),
-            capacity,
+            buffer: [T::default(); N],
             head: 0,
             tail: 0,
             size: 0,
@@ -23,19 +17,14 @@ impl<T> CircularBuffer<T> {
     }
 
     pub fn push(&mut self, item: T) {
-        if self.size == self.capacity {
-            self.tail = (self.tail + 1) % self.capacity;
+        if self.size == N {
+            self.tail = (self.tail + 1) % N;
         } else {
             self.size += 1;
         }
 
-        if self.buffer.len() < self.capacity {
-            self.buffer.push(item);
-        } else {
-            self.buffer[self.head] = item;
-        }
-
-        self.head = (self.head + 1) % self.capacity;
+        self.buffer[self.head] = item;
+        self.head = (self.head + 1) % N;
     }
 
     pub fn pop(&mut self) -> Option<T> {
@@ -43,8 +32,8 @@ impl<T> CircularBuffer<T> {
             return None;
         }
 
-        let item = std::mem::replace(&mut self.buffer[self.tail], unsafe { std::mem::zeroed() });
-        self.tail = (self.tail + 1) % self.capacity;
+        let item = self.buffer[self.tail];
+        self.tail = (self.tail + 1) % N;
         self.size -= 1;
 
         Some(item)
@@ -64,12 +53,6 @@ impl<T> CircularBuffer<T> {
         self.size = 0;
     }
 
-    pub fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-        for item in iter {
-            self.push(item);
-        }
-    }
-
     pub fn len(&self) -> usize {
         self.size
     }
@@ -79,44 +62,40 @@ impl<T> CircularBuffer<T> {
     }
 
     pub fn is_full(&self) -> bool {
-        self.size == self.capacity
+        self.size == N
     }
 
     pub fn capacity(&self) -> usize {
-        self.capacity
+        N
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         (0..self.size).map(move |i| {
-            let index = (self.tail + i) % self.capacity;
+            let index = (self.tail + i) % N;
             &self.buffer[index]
         })
     }
 }
 
-impl<T> Default for CircularBuffer<T>
-where
-    T: Default + Clone,
-{
+impl<T: Default + Copy, const N: usize> Default for CircularBuffer<T, N> {
     fn default() -> Self {
-        Self {
-            buffer: Vec::new(),
-            capacity: 0,
-            head: 0,
-            tail: 0,
-            size: 0,
-        }
+        Self::new()
     }
 }
 
-impl<T> FromIterator<T> for CircularBuffer<T> {
+impl<T: Default + Copy, const N: usize> FromIterator<T> for CircularBuffer<T, N> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        let iter = iter.into_iter();
-        let capacity = iter.size_hint().0;
-        let mut buffer = Self::new(capacity);
+        let mut buffer = Self::new();
         buffer.extend(iter);
-
         buffer
+    }
+}
+
+impl<T: Default + Copy, const N: usize> Extend<T> for CircularBuffer<T, N> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for item in iter {
+            self.push(item);
+        }
     }
 }
 
@@ -125,105 +104,96 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let buffer: CircularBuffer<i32> = CircularBuffer::new(5);
-        assert_eq!(buffer.capacity(), 5);
-        assert_eq!(buffer.len(), 0);
-        assert!(buffer.is_empty());
-        assert!(!buffer.is_full());
-    }
-
-    #[test]
     fn test_push_and_pop() {
-        let mut buffer = CircularBuffer::new(3);
+        let mut buffer = CircularBuffer::<i32, 3>::new();
         buffer.push(1);
         buffer.push(2);
         buffer.push(3);
-        assert_eq!(buffer.len(), 3);
-        assert!(!buffer.is_empty());
-        assert!(buffer.is_full());
 
         assert_eq!(buffer.pop(), Some(1));
         assert_eq!(buffer.pop(), Some(2));
         assert_eq!(buffer.pop(), Some(3));
         assert_eq!(buffer.pop(), None);
-        assert!(buffer.is_empty());
+    }
+
+    #[test]
+    fn test_push_overflow() {
+        let mut buffer = CircularBuffer::<i32, 3>::new();
+        buffer.push(1);
+        buffer.push(2);
+        buffer.push(3);
+        buffer.push(4);
+
+        assert_eq!(buffer.pop(), Some(2));
+        assert_eq!(buffer.pop(), Some(3));
+        assert_eq!(buffer.pop(), Some(4));
+        assert_eq!(buffer.pop(), None);
     }
 
     #[test]
     fn test_peek() {
-        let mut buffer = CircularBuffer::new(3);
+        let mut buffer = CircularBuffer::<i32, 3>::new();
         buffer.push(1);
         buffer.push(2);
+
         assert_eq!(buffer.peek(), Some(&1));
+
         buffer.push(3);
         assert_eq!(buffer.peek(), Some(&1));
 
         buffer.pop();
         assert_eq!(buffer.peek(), Some(&2));
-        buffer.pop();
-        assert_eq!(buffer.peek(), Some(&3));
-        buffer.pop();
-        assert_eq!(buffer.peek(), None);
     }
 
     #[test]
     fn test_clear() {
-        let mut buffer = CircularBuffer::new(3);
+        let mut buffer = CircularBuffer::<i32, 3>::new();
         buffer.push(1);
         buffer.push(2);
         buffer.push(3);
         buffer.clear();
+
         assert_eq!(buffer.len(), 0);
         assert!(buffer.is_empty());
         assert!(!buffer.is_full());
     }
 
     #[test]
-    fn test_extend() {
-        let mut buffer = CircularBuffer::new(5);
-        buffer.extend(vec![1, 2, 3]);
-        assert_eq!(buffer.len(), 3);
-        assert_eq!(buffer.pop(), Some(1));
-        assert_eq!(buffer.pop(), Some(2));
-        assert_eq!(buffer.pop(), Some(3));
-        assert_eq!(buffer.pop(), None);
-
-        buffer.extend(vec![4, 5, 6, 7]);
-        assert_eq!(buffer.len(), 4);
-        assert_eq!(buffer.pop(), Some(4));
-        assert_eq!(buffer.pop(), Some(5));
-        assert_eq!(buffer.pop(), Some(6));
-        assert_eq!(buffer.pop(), Some(7));
-        assert_eq!(buffer.pop(), None);
+    fn test_capacity() {
+        let buffer = CircularBuffer::<i32, 5>::new();
+        assert_eq!(buffer.capacity(), 5);
     }
 
     #[test]
     fn test_iter() {
-        let mut buffer = CircularBuffer::new(5);
+        let mut buffer = CircularBuffer::<i32, 5>::new();
         buffer.push(1);
         buffer.push(2);
         buffer.push(3);
-
-        let collected: Vec<&i32> = buffer.iter().collect();
-        assert_eq!(collected, vec![&1, &2, &3]);
-
         buffer.push(4);
         buffer.push(5);
+
         let collected: Vec<&i32> = buffer.iter().collect();
-        assert_eq!(collected, vec![&3, &4, &5]);
+        assert_eq!(collected, vec![&1, &2, &3, &4, &5]);
+
+        buffer.pop();
+        buffer.push(6);
+
+        let collected: Vec<&i32> = buffer.iter().collect();
+        assert_eq!(collected, vec![&2, &3, &4, &5, &6]);
     }
 
     #[test]
     fn test_from_iterator() {
-        let mut buffer: CircularBuffer<i32> = vec![1, 2, 3, 4, 5].into_iter().collect();
-        assert_eq!(buffer.capacity(), 5);
-        assert_eq!(buffer.len(), 5);
-        assert_eq!(buffer.pop(), Some(1));
-        assert_eq!(buffer.pop(), Some(2));
-        assert_eq!(buffer.pop(), Some(3));
-        assert_eq!(buffer.pop(), Some(4));
-        assert_eq!(buffer.pop(), Some(5));
-        assert_eq!(buffer.pop(), None);
+        let buffer: CircularBuffer<i32, 4> = vec![1, 2, 3].into_iter().collect();
+        assert_eq!(buffer.len(), 3);
+        assert_eq!(buffer.iter().cloned().collect::<Vec<i32>>(), vec![1, 2, 3]);
+
+        let buffer: CircularBuffer<i32, 4> = vec![1, 2, 3, 4, 5].into_iter().collect();
+        assert_eq!(buffer.len(), 4);
+        assert_eq!(
+            buffer.iter().cloned().collect::<Vec<i32>>(),
+            vec![2, 3, 4, 5]
+        );
     }
 }
